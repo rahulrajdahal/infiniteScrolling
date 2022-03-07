9ef2c5dbbc415226a9ed8bb853f4173c0c4431d0
Markaa10 <rahulrajdahal@gmail.com>
2022-03-07 14:22:15 +0545

    inittial

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>App Typescript</Text>
+    </View>
+  );
+}
+
+export default App;
